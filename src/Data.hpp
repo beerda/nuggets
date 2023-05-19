5de7f1b3c8dfa85e6e0b811e1ce3e6ff6e393024
Michal Burda <michal.burda@osu.cz>
2023-05-19 10:39:49 +0200

    Added Data

---
+++ src/Data.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Chain.hpp"
+
+
+class Data {
+public:
+    Data()
+    { }
+
+    template <typename T>
+    void addChain(const T& values)
+    {
+        if (chains.size() > 0) {
+            if (values.size() != chains.front().size()) {
+                throw new runtime_error("Chain sizes mismatch in Data::addChain");
+            }
+        }
+
+        chains.push_back(Chain(values));
+    }
+
+    const Chain& getChain(size_t i) const
+    { return chains.at(i); }
+
+private:
+    vector<Chain> chains;
+};
