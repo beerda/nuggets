7ab5dee5b871c28c75581c315bc680f5bbdfe47a
Michal Burda <michal.burda@osu.cz>
2023-05-22 15:31:04 +0200

    Doc

--- src/Argumentator.hpp
+++ src/Argumentator.hpp
@@ -3,6 +3,10 @@
 #include "Task.hpp"
 
 
+/**
+ * This abstract class represents objects responsible for initialization of arguments for
+ * the R callback function that is called on each generated condition.
+ */
 class Argumentator {
 public:
     virtual void prepare(writable::list& arguments, const Task& task) const
--- src/Filter.hpp
+++ src/Filter.hpp
@@ -3,6 +3,9 @@
 #include "Task.hpp"
 
 
+/**
+ * This abstract class represents an extension that may filter the conditions being generated.
+ */
 class Filter {
 public:
     virtual bool isRedundant(const Task& task) const
