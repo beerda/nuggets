29ae23b5839eaab819b5c0c40c25c9813e86ae95
Michal Burda <michal.burda@osu.cz>
2023-05-22 15:34:41 +0200

    Test dig() - use of condition arg

---
+++ tests/testthat/test-dig.R
@@ -0,0 +1,19 @@
+test_that("empty arg", {
+    m <- matrix(1:12 / 12, ncol = 2)
+    res <- dig(m, function() 1)
+
+    expect_equal(length(res), 4)
+    expect_equal(res, rep(list(1), 4))
+})
+
+
+test_that("condition arg", {
+    m <- matrix(1:12 / 12, ncol = 2)
+    res <- dig(m, function(condition) list(cond = condition))
+
+    expect_equal(length(res), 4)
+    expect_equal(res, list(list(cond = integer(0)),
+                           list(cond = c(1L)),
+                           list(cond = c(2L)),
+                           list(cond = c(1L, 2L))))
+})
