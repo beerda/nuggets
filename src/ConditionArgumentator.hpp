12b9efb0fb23af78b3614c059d8f5cfaf2386c42
Michal Burda <michal.burda@osu.cz>
2023-05-22 15:33:45 +0200

    Translate condition indices in ConditionArgumentator

--- src/ConditionArgumentator.hpp
+++ src/ConditionArgumentator.hpp
@@ -3,16 +3,26 @@
 #include "Argumentator.hpp"
 
 
+/**
+ * Prepare the 'condition' argument for the R function callback. The 'condition' argument
+ * is an integer vector of column indices, which represents the conjunctive condition.
+ */
 class ConditionArgumentator : public Argumentator {
 public:
+    ConditionArgumentator(const integers& predicates)
+        : predicates(predicates)
+    { }
+
     void prepare(writable::list& arguments, const Task& task) const override
     {
         using namespace cpp11::literals;
 
         writable::integers condition;
         for (int p : task.getCurrentCondition()) {
-            condition.push_back(p);
+            condition.push_back(predicates[p]);
         }
         arguments.push_back("condition"_nm = condition);
     }
+private:
+    integers predicates;
 };
