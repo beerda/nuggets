140a6c9a83fffcc114dc393ec8bb7aa4477842b4
Michal Burda <michal.burda@osu.cz>
2023-05-23 12:30:54 +0200

    Import methods::formalArgs

--- R/nuggets-package.R
+++ R/nuggets-package.R
@@ -4,6 +4,7 @@
 ## usethis namespace: start
 #' @importFrom assertthat assert_that
 #' @importFrom assertthat is.number
+#' @importFrom methods formalArgs
 #' @useDynLib nuggets, .registration = TRUE
 ## usethis namespace: end
 NULL
