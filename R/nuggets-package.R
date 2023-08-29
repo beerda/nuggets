b7a7228a8a45401912af073d684c91f585b6fb64
Michal Burda <michal.burda@osu.cz>
2023-08-29 14:17:26 +0200

    Added imports

--- R/nuggets-package.R
+++ R/nuggets-package.R
@@ -2,17 +2,21 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom assertthat assert_that
-#' @importFrom assertthat is.number
 #' @importFrom cli cli_abort
 #' @importFrom methods formalArgs
 #' @importFrom rlang caller_env
+#' @importFrom rlang current_env
 #' @importFrom rlang enquo
+#' @importFrom rlang enquos
 #' @importFrom rlang is_integer
 #' @importFrom rlang is_integerish
+#' @importFrom rlang is_scalar_atomic
+#' @importFrom rlang is_scalar_character
 #' @importFrom rlang is_scalar_double
 #' @importFrom rlang is_scalar_integerish
+#' @importFrom rlang is_scalar_logical
 #' @importFrom tidyselect eval_select
+#' @importFrom tidyselect everything
 #' @useDynLib nuggets, .registration = TRUE
 ## usethis namespace: end
 NULL
