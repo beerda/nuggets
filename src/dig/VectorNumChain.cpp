7793cbe485f80ed2f24d91783e109508a9922815
Michal Burda <michal.burda@osu.cz>
2023-11-05 17:30:17 +0100

    Added tests for VectorNumChain

--- src/dig/VectorNumChain.cpp
+++ src/dig/VectorNumChain.cpp
@@ -1,12 +1,32 @@
+#include <algorithm>
 #include "dig/VectorNumChain.h"
 
 
+template <>
+void VectorNumChain<TNorm::GODEL>::conjunctWith(const VectorNumChain<TNorm::GODEL>& other)
+{
+    cachedSum = 0;
+    for (size_t i = 0; i < values.size(); i++) {
+        values[i] = min(values[i], other.values[i]);
+        cachedSum += values[i];
+    }
+}
 template <>
 void VectorNumChain<TNorm::GOGUEN>::conjunctWith(const VectorNumChain<TNorm::GOGUEN>& other)
 {
     cachedSum = 0;
     for (size_t i = 0; i < values.size(); i++) {
         values[i] *= other.values[i];
+        cachedSum += values[i];
+    }
+}
+
+template <>
+void VectorNumChain<TNorm::LUKASIEWICZ>::conjunctWith(const VectorNumChain<TNorm::LUKASIEWICZ>& other)
+{
+    cachedSum = 0;
+    for (size_t i = 0; i < values.size(); i++) {
+        values[i] = max(0.0, values[i] + other.values[i] - 1);
         cachedSum += values[i];
     }
 }
---
+++ src/test-dig-VectorNumChain.cpp
@@ -0,0 +1,79 @@
+#include <testthat.h>
+#include "common.h"
+#include "dig/VectorNumChain.h"
+
+context("dig/VectorNumChain.h") {
+    test_that("initializations") {
+        VectorNumChain<GOGUEN> chain;
+        expect_true(chain.empty());
+        expect_true(chain.size() == 0);
+        expect_true(chain.getSum() == 0);
+
+        chain.push_back(0);
+        expect_false(chain.empty());
+        expect_true(chain.size() == 1);
+        expect_true(chain.getSum() == 0);
+        expect_true(chain.at(0) == 0.0);
+
+        chain.push_back(0.5);
+        expect_false(chain.empty());
+        expect_true(chain.size() == 2);
+        expect_true(chain.getSum() == 0.5);
+        expect_true(chain.at(0) == 0.0);
+        expect_true(chain.at(1) == 0.5);
+
+        chain.push_back(1);
+        expect_false(chain.empty());
+        expect_true(chain.size() == 3);
+        expect_true(chain.getSum() == 1.5);
+        expect_true(chain.at(0) == 0.0);
+        expect_true(chain.at(1) == 0.5);
+        expect_true(chain.at(2) == 1.0);
+
+    }
+
+    test_that("GODEL") {
+        VectorNumChain<GODEL> chain;
+        chain.push_back(0.0);
+        chain.push_back(0.5);
+        chain.push_back(1.0);
+
+        VectorNumChain<GODEL> other;
+        other.push_back(0.8);
+        other.push_back(0.6);
+        other.push_back(0.4);
+
+        chain.conjunctWith(other);
+        expect_true(chain.getSum() == 0.0 + 0.5 + 0.4);
+    }
+
+    test_that("GOGUEN") {
+        VectorNumChain<GOGUEN> chain;
+        chain.push_back(0.0);
+        chain.push_back(0.5);
+        chain.push_back(1.0);
+
+        VectorNumChain<GOGUEN> other;
+        other.push_back(0.8);
+        other.push_back(0.6);
+        other.push_back(0.4);
+
+        chain.conjunctWith(other);
+        expect_true(chain.getSum() == 0.0 + 0.3 + 0.4);
+    }
+
+    test_that("LUKASIEWICZ") {
+        VectorNumChain<LUKASIEWICZ> chain;
+        chain.push_back(0.0);
+        chain.push_back(0.5);
+        chain.push_back(1.0);
+
+        VectorNumChain<LUKASIEWICZ> other;
+        other.push_back(0.8);
+        other.push_back(0.6);
+        other.push_back(0.4);
+
+        chain.conjunctWith(other);
+        expect_true(chain.getSum() == 0 + 0.1 + 0.4);
+    }
+}
