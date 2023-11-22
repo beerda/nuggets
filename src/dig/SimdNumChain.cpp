ba3ae5825d10d2a19d7086fa4decb82e4b015879
Michal Burda <michal.burda@centrum.cz>
2023-11-22 18:05:50 +0100

    Refactored SimdNumChain and implemented GOEDEL + LUKASIEWICZ

--- src/dig/SimdNumChain.cpp
+++ src/dig/SimdNumChain.cpp
@@ -1,57 +1,45 @@
+#include <cmath>
 #include "dig/SimdNumChain.h"
 
 
 template <>
 void SimdNumChain<TNorm::GOEDEL>::conjunctWith(const SimdNumChain<TNorm::GOEDEL>& other)
 {
-        throw std::invalid_argument("SimdNumChain::conjunctWith: incompatible sizes");
-    if (values.size() != other.values.size()) {
-    }
+    const auto batchOp = [] (const batchType& a, const batchType& b, batchType& res) {
+        res = fmin(a, b);
+    };
+    const auto seqOp = [] (float a, float b) {
+        return fmin(a, b);
+    };
 
-    cachedSum = 0;
-    for (size_t i = 0; i < values.size(); i++) {
-        values[i] = min(values[i], other.values[i]);
-        cachedSum += values[i];
-    }
+    batchConjunct(other.values, batchOp, seqOp);
 }
 
 template <>
 void SimdNumChain<TNorm::GOGUEN>::conjunctWith(const SimdNumChain<TNorm::GOGUEN>& other)
 {
-    if (values.size() != other.values.size()) {
-        throw std::invalid_argument("SimdNumChain::conjunctWith: incompatible sizes");
-    }
+    const auto batchOp = [] (const batchType& a, const batchType& b, batchType& res) {
+        res = a * b;
+    };
+    const auto seqOp = [] (float a, float b) {
+        return a * b;
+    };
 
-    cachedSum = 0;
-
-    using batchType = xs::batch<float>;
-    size_t batchSize = batchType::size;
-    size_t simdSize = values.size() - values.size() % batchSize;
-
-    for (size_t i = 0; i < simdSize; i += batchSize) {
-        batchType a = batchType::load_unaligned(&values[i]);
-        batchType b = batchType::load_unaligned(&other.values[i]);
-        batchType res = a * b;
-        res.store_unaligned(&values[i]);
-        cachedSum += xs::reduce_add(res);
-    }
-
-    for (size_t i = simdSize; i < values.size(); ++i) {
-        values[i] *= other.values[i];
-        cachedSum += values[i];
-    }
+    batchConjunct(other.values, batchOp, seqOp);
 }
 
 template <>
 void SimdNumChain<TNorm::LUKASIEWICZ>::conjunctWith(const SimdNumChain<TNorm::LUKASIEWICZ>& other)
 {
-        throw std::invalid_argument("SimdNumChain::conjunctWith: incompatible sizes");
-    if (values.size() != other.values.size()) {
-    }
+    const batchType zero(0.0f);
+    const batchType one(1.0f);
 
-    cachedSum = 0;
-    for (size_t i = 0; i < values.size(); i++) {
-        values[i] = max(0.0f, values[i] + other.values[i] - 1);
-        cachedSum += values[i];
-    }
+    const auto batchOp = [&zero, &one] (const batchType& a, const batchType& b, batchType& res) {
+        res = fmax(zero, a + b - one);
+    };
+    const auto seqOp = [] (float a, float b) {
+        return fmax(0.0f, a + b - 1);
+    };
+
+    batchConjunct(other.values, batchOp, seqOp);
 }
--- src/dig/SimdNumChain.h
+++ src/dig/SimdNumChain.h
@@ -1,14 +1,18 @@
 #pragma once
 
+#include <functional>
 #include "AbstractVectorNumChain.h"
 
 
 /**
- * Implementation of chain of numbers based on a vector.
- */
+* Implementation of chain of numbers based on a vector with operations
+* implemented using SIMD instructions.
+*/
 template <TNorm TNORM>
 class SimdNumChain : public AbstractVectorNumChain {
 public:
+    using batchType = xs::batch<float>;
+
     SimdNumChain()
         : AbstractVectorNumChain()
     { }
@@ -24,4 +28,33 @@ public:
 
     bool operator != (const SimdNumChain& other) const
     { return !(*this == other); }
+
+private:
+    size_t batchSize = batchType::size;
+
+    void batchConjunct(const vector<float> otherValues,
+                       const std::function<void (const batchType&, const batchType&, batchType&)>& batchOp,
+                       const std::function<float (float, float)>& seqOp)
+    {
+        if (values.size() != otherValues.size()) {
+            throw std::invalid_argument("SimdNumChain::conjunctWith: incompatible sizes");
+        }
+
+        cachedSum = 0;
+        size_t simdSize = values.size() - values.size() % batchSize;
+
+        for (size_t i = 0; i < simdSize; i += batchSize) {
+            batchType a = batchType::load_unaligned(&values[i]);
+            batchType b = batchType::load_unaligned(&otherValues[i]);
+            batchType res;
+            batchOp(a, b, res);
+            res.store_unaligned(&values[i]);
+            cachedSum += xs::reduce_add(res);
+        }
+
+        for (size_t i = simdSize; i < values.size(); ++i) {
+            values[i] = seqOp(values[i], otherValues[i]);
+            cachedSum += values[i];
+        }
+    }
 };
