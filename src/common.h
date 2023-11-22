95780fc2aa9813eaf0fcb5328bf351e437e0681c
Michal Burda <michal.burda@centrum.cz>
2023-11-22 16:39:09 +0100

    Added xsimd library

--- src/common.h
+++ src/common.h
@@ -4,9 +4,12 @@
 
 #include <cmath>
 #include <Rcpp.h>
+#include "xsimd/xsimd.hpp"
 
 using namespace Rcpp;
 using namespace std;
+namespace xs = xsimd;
+
 
 #define EQUAL(a, b) (fabs((a) - (b)) < 1e-6)
 
