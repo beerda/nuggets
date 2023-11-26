d5cff41c131fb7c105de9b46cb725a448e907a21
Michal Burda <michal.burda@osu.cz>
2023-11-26 14:23:33 +0100

    Simd-version must not be implemented using lambda functions

--- src/dig/SimdVectorNumChain.cpp
+++ src/dig/SimdVectorNumChain.cpp
@@ -5,41 +5,73 @@
 template <>
 void SimdVectorNumChain<TNorm::GOEDEL>::conjunctWith(const SimdVectorNumChain<TNorm::GOEDEL>& other)
 {
-    const auto batchOp = [] (const batchType& a, const batchType& b, batchType& res) {
-        res = fmin(a, b);
-    };
-    const auto seqOp = [] (float a, float b) {
-        return fmin(a, b);
-    };
+    if (values.size() != other.values.size()) {
+        throw std::invalid_argument("SimdVectorNumChain::conjunctWith: incompatible sizes");
+    }
 
-    batchConjunct(other.values, batchOp, seqOp);
+    cachedSum = 0;
+    size_t simdSize = values.size() - values.size() % batchSize;
+
+    for (size_t i = 0; i < simdSize; i += batchSize) {
+        batchType a = batchType::load_unaligned(&values[i]);
+        batchType b = batchType::load_unaligned(&other.values[i]);
+        batchType res = fmin(a, b);
+        res.store_unaligned(&values[i]);
+        cachedSum += xsimd::reduce_add(res);
+    }
+
+    for (size_t i = simdSize; i < values.size(); ++i) {
+        values[i] = fmin(values[i], other.values[i]);
+        cachedSum += values[i];
+    }
 }
 
 template <>
 void SimdVectorNumChain<TNorm::GOGUEN>::conjunctWith(const SimdVectorNumChain<TNorm::GOGUEN>& other)
 {
-    const auto batchOp = [] (const batchType& a, const batchType& b, batchType& res) {
-        res = a * b;
-    };
-    const auto seqOp = [] (float a, float b) {
-        return a * b;
-    };
+    if (values.size() != other.values.size()) {
+        throw std::invalid_argument("SimdVectorNumChain::conjunctWith: incompatible sizes");
+    }
 
-    batchConjunct(other.values, batchOp, seqOp);
+    cachedSum = 0;
+    size_t simdSize = values.size() - values.size() % batchSize;
+
+    for (size_t i = 0; i < simdSize; i += batchSize) {
+        batchType a = batchType::load_unaligned(&values[i]);
+        batchType b = batchType::load_unaligned(&other.values[i]);
+        batchType res  = a * b;
+        res.store_unaligned(&values[i]);
+        cachedSum += xsimd::reduce_add(res);
+    }
+
+    for (size_t i = simdSize; i < values.size(); ++i) {
+        values[i] = values[i] * other.values[i];
+        cachedSum += values[i];
+    }
 }
 
 template <>
 void SimdVectorNumChain<TNorm::LUKASIEWICZ>::conjunctWith(const SimdVectorNumChain<TNorm::LUKASIEWICZ>& other)
 {
+    if (values.size() != other.values.size()) {
+        throw std::invalid_argument("SimdVectorNumChain::conjunctWith: incompatible sizes");
+    }
+
     const batchType zero(0.0f);
     const batchType one(1.0f);
+    cachedSum = 0;
+    size_t simdSize = values.size() - values.size() % batchSize;
 
-    const auto batchOp = [&zero, &one] (const batchType& a, const batchType& b, batchType& res) {
-        res = fmax(zero, a + b - one);
-    };
-    const auto seqOp = [] (float a, float b) {
-        return fmax(0.0f, a + b - 1);
-    };
+    for (size_t i = 0; i < simdSize; i += batchSize) {
+        batchType a = batchType::load_unaligned(&values[i]);
+        batchType b = batchType::load_unaligned(&other.values[i]);
+        batchType res = fmax(zero, a + b - one);
+        res.store_unaligned(&values[i]);
+        cachedSum += xsimd::reduce_add(res);
+    }
 
-    batchConjunct(other.values, batchOp, seqOp);
+    for (size_t i = simdSize; i < values.size(); ++i) {
+        values[i] = fmax(0.0f, values[i] + other.values[i] - 1);
+        cachedSum += values[i];
+    }
 }
