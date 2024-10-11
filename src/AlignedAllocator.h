f74682d57b3d4664b11d12b5095e4c8dbdccdae0
Michal Burda <michal.burda@osu.cz>
2024-10-11 13:51:43 +0200

    Trying another implementation of AlignedAllocator

--- src/AlignedAllocator.h
+++ src/AlignedAllocator.h
@@ -1,60 +1,130 @@
 #pragma once
 
-#include <memory>
-#include <stdlib.h>
+#include <cstdlib>
 
-// A minimal implementation of an allocator for C++ Standard Library, which
-// allocates aligned memory (specified by the alignment argument).
-// Note:
-//    A minimal custom allocator is preferred because C++ allocator_traits class
-//    provides default implementation for you. Take a look at Microsoft's
-//    documentation about Allocators and allocator class.
-template <typename T, std::size_t alignment> class AlignedAllocator {
+// alignedMalloc/alignedFree implementations found at
+// http://www.gamasutra.com/view/feature/3942/data_alignment_part_1.php?page=2
+
+/**
+ * \brief Allocate memory aligned at on a value.  Memory allocated through alignedMalloc() \b must be
+ * freed through alignedFree()
+ * \param size The amount of memory to allocate
+ * \param alignment The value to align on
+ */
+inline void* alignedMalloc(size_t size, size_t alignment)
+{
+    const int pointerSize = sizeof(void*);
+    const int requestedSize = size + alignment - 1 + pointerSize;
+    void* raw = std::malloc(requestedSize);
+
+    if (!raw)
+    {
+        return 0;
+    }
+
+    void* start = (uint8_t*)raw + pointerSize;
+    void* aligned = (void*)(((uintptr_t)((uint8_t*)start+alignment-1)) & ~(alignment-1));
+    *(void**)((uint8_t*)aligned-pointerSize) = raw;
+    return aligned;
+}
+
+/**
+ * \brief Free memory allocated through alignedMalloc()
+ * \param aligned The memory to free
+ */
+inline void alignedFree(void* aligned)
+{
+    if (!aligned)
+    {
+        return;
+    }
+
+    void* raw = *(void**)((uint8_t*)aligned - sizeof(void*));
+    std::free(raw);
+}
+
+template<class T, size_t align> class AlignedAllocator;
+
+// specialize for void:
+template<size_t align>
+class AlignedAllocator<void, align>
+{
 public:
-    using value_type = T;
+    typedef void* pointer;
+    typedef const void* const_pointer;
+    // reference to void members are impossible.
+    typedef void value_type;
 
+    template<class U>
+    struct rebind
+    {
+        typedef AlignedAllocator<U, align> other;
+    };
+};
+
+/**
+ * \brief An stl-compatible aligned allocator
+ * \param T The type of the container element
+ * \param align The alignment to allocate on
+ */
+template<class T, size_t align>
+class AlignedAllocator
+{
 public:
-    // According to Microsoft's documentation, default constructor is not required
-    // by C++ Standard Library.
-    AlignedAllocator() noexcept {};
+    typedef size_t size_type;
+    typedef ptrdiff_t difference_type;
+    typedef T* pointer;
+    typedef const T* const_pointer;
+    typedef T& reference;
+    typedef const T& const_reference;
+    typedef T value_type;
 
-    template <typename U> AlignedAllocator(const AlignedAllocator<U, alignment>& other) noexcept {};
+    template<class U>
+    struct rebind
+    { typedef AlignedAllocator<U, align> other; };
 
     template <typename U>
-    inline bool operator==(const AlignedAllocator<U, alignment>& other) const noexcept {
+    inline bool operator==(const AlignedAllocator<U, align>& other) const noexcept {
         return true;
     }
 
     template <typename U>
-    inline bool operator!=(const AlignedAllocator<U, alignment>& other) const noexcept {
+    inline bool operator!=(const AlignedAllocator<U, align>& other) const noexcept {
         return false;
     }
 
-    template <typename U> struct rebind {
-        using other = AlignedAllocator<U, alignment>;
-    };
+    AlignedAllocator() throw ()
+    { }
 
-    // STL containers call this function to allocate uninitialized memory block to
-    // store (no more than n) elements of type T (value_type).
-    inline value_type* allocate(const std::size_t n) const {
-        auto size = n;
-        /*
-         If you wish, for some strange reason, that the size of allocated buffer is
-         also aligned to alignment, uncomment the following statement.
+    ~AlignedAllocator() throw ()
+    { }
 
-         Note: this increases the size of underlying memory, but STL containers
-         still treat it as a memory block of size n, i.e., STL containers will not
-         put more than n elements into the returned memory.
-         */
-        // size = (n + alignment - 1) / alignment * alignment;
-        value_type* ptr;
-        auto ret = posix_memalign((void**)&ptr, alignment, sizeof(T) * size);
-        if (ret != 0)
+    pointer address(reference r) const
+    { return &r; }
+
+    const_pointer address(const_reference r) const
+    { return &r; }
+
+    size_type max_size() const throw ()
+    { return ~size_type(0); }
+
+    pointer allocate(size_type n, typename AlignedAllocator<void, align>::const_pointer hint = 0)
+    {
+        void* mem = alignedMalloc(n * sizeof(T), align);
+        if (!mem)
+        {
             throw std::bad_alloc();
-        return ptr;
-    };
+        }
 
-    // STL containers call this function to free a memory block beginning at a
-    // specified position.
-    inline void deallocate(value_type* const ptr, std::size_t n) const noexcept { free(ptr); }
+        return static_cast<pointer>(mem);
+    }
+
+    void deallocate(pointer p, size_type n)
+    { alignedFree(p); }
+
+    void construct(pointer p, const_reference val)
+    { new (p) T(val); }
+
+    void destroy(pointer p)
+    { p->~T(); }
 };
