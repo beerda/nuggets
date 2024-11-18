3f502eb819a12be78e38b10affffa2c6edb1523e
Michal Burda <michal.burda@osu.cz>
2024-11-18 15:35:47 +0100

    Updated documentation

--- R/dig_contrasts.R
+++ R/dig_contrasts.R
@@ -2,12 +2,26 @@
 #'
 #' Contrast patterns are a generalization of association rules that allow
 #' for the specification of a condition under which there is a significant
-#' difference between two numeric variables. The contrast is computed using
+#' difference in some statistical feature between two numeric variables.
+#'
+#' \describe{
+#'   \item{Scheme:}{`theta(xvar) >> theta(yvar) | C`\cr\cr
+#'     The feature `theta` of the first variable `xvar` is significantly higher
+#'     than the feature `theta` of the second variable `yvar` under the
+#'     condition `C`.}
+#'   \item{Example:}{`mean(daily_ice_cream_income) >> mean(daily_tea_income) | sunny`\cr\cr
+#'     The *mean* of *daily ice-cream income* is significantly higher than
+#'     the *mean* of *daily tea income* under the condition of *sunny weather*.}
+#' }
+#'
+#' The contrast is computed using
 #' a statistical test, which is specified by the `method` argument. The
 #' function computes the contrast between all pairs of variables, where the
 #' first variable is specified by the `xvars` argument and the second variable
 #' is specified by the `yvars` argument. The contrast is computed in sub-data
-#' corresponding to conditions generated from the `condition` columns.
+#' corresponding to conditions generated from the `condition` columns. The
+#' `dig_contrasts()` function supports crisp conditions only, i.e., the
+#' condition columns must be logical.
 #'
 #' @param x a matrix or data frame with data to search in.
 #' @param condition a tidyselect expression (see
@@ -45,7 +59,48 @@
 #' @param ... Further arguments passed to the underlying test function
 #'      ([t.test()], [wilcox.test()], or [var.test()] accordingly to the
 #'      selected method).
-#' @return A tibble with found rules.
+#' @return A tibble with found patterns in rows. The following columns are always
+#'      present:
+#'      \item{condition}{the condition of the pattern as a character string
+#'        in the form `{p1 & p2 & ... & pn}` where `p1`, `p2`, ..., `pn` are
+#'        `x`'s column names.}
+#'      \item{support}{the support of the condition, i.e., the relative
+#'        frequency of the condition in the dataset `x`.}
+#'      \item{xvar}{the name of the first variable in the contrast.}
+#'      \item{yvar}{the name of the second variable in the contrast.}
+#'      \item{p_value}{the p-value of the underlying test.}
+#'      \item{rows}{the number of rows in the sub-data corresponding to
+#'        the condition.}
+#'      \item{alternative}{a character string indicating the alternative
+#'        hypothesis.}
+#'      \item{method}{a character string indicating the method used for the
+#'        test.}
+#'      For the `"t"` method, the following additional columns are also
+#'      present (see also [t.test()]):
+#'      \item{estimate_x}{the estimated mean of variable `xvar`.}
+#'      \item{estimate_y}{the estimated mean of variable `yvar`.}
+#'      \item{t_statistic}{the t-statistic of the t test.}
+#'      \item{df}{the degrees of freedom of the t test.}
+#'      \item{conf_int_lo}{the lower bound of the confidence interval.}
+#'      \item{conf_int_hi}{the upper bound of the confidence interval.}
+#'      \item{stderr}{the standard error of the mean difference.}
+#'      For the `"wilcox"` method, the following additional columns are also
+#'      present (see also [wilcox.test()]):
+#'      \item{estimate}{the estimate of the location parameter.}
+#'      \item{W_statistic}{the Wilcoxon rank sum statistic.}
+#'      \item{conf_int_lo}{the lower bound of the confidence interval.}
+#'      \item{conf_int_hi}{the upper bound of the confidence interval.}
+#'      For the `"var"` method, the following additional columns are also
+#'      present (see also [var.test()]):
+#'      \item{estimate}{the ratio of the sample variances of variables
+#'        `xvar` and `yvar`.}
+#'      \item{F_statistic}{the value of the F test statistic.}
+#'      \item{df1}{the numerator degrees of freedom.}
+#'      \item{df2}{the denominator degrees of freedom.}
+#'      \item{conf_int_lo}{the lower bound of the confidence interval for the
+#'        ratio of the population variances.}
+#'      \item{conf_int_hi}{the upper bound of the confidence interval for the
+#'        ratio of the population variances.}
 #' @author Michal Burda
 #' @seealso [dig()], [dig_grid()], [stats::t.test()], [stats::wilcox.test()], [stats::var.test()]
 #' @examples
@@ -56,7 +111,6 @@
 #'               yvars = uptake,
 #'               method = "t",
 #'               min_support = 0.1)
-#'
 #' @export
 dig_contrasts <- function(x,
                           condition = where(is.logical),
