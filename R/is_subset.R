1feee64f20d0d8a17cdcc51cc22b2f3276834756
Michal Burda <michal.burda@osu.cz>
2024-11-23 10:55:32 +0100

    Polishing is_subset()

--- R/is_subset.R
+++ R/is_subset.R
@@ -2,7 +2,9 @@
 #'
 #' @param x the first vector
 #' @param y the second vector
-#' @return TRUE if `x` is a subset of `y` or FALSE otherwise.
+#' @return `TRUE` if `x` is a subset of `y`, or `FALSE` otherwise. `x` is
+#'      considered a subset of `y` if all elements of `x` are also in `y`,
+#'      i.e., if `setdiff(x, y)` is a vector of length 0.
 #' @author Michal Burda
 #' @export
 is_subset <- function(x, y) {
--- tests/testthat/test-is_subset.R
+++ tests/testthat/test-is_subset.R
@@ -1,5 +1,13 @@
 test_that("is_subset", {
-  expect_true(is_subset(3:5, 1:8))
-  expect_true(is_subset(3:5, 3:5))
-  expect_false(is_subset(2:5, 3:5))
+    expect_true(is_subset(c(), c()))
+    expect_true(is_subset(c(), 1:5))
+    expect_true(is_subset(3:5, 1:8))
+    expect_true(is_subset(3:5, 3:5))
+
+    expect_false(is_subset(2:5, c()))
+    expect_false(is_subset(2:5, 3:5))
+
+    expect_error(is_subset(list(), list()))
+    expect_error(is_subset(matrix(0, nrow = 3, ncol = 3),
+                           matrix(0, nrow = 3, ncol = 3)))
 })
