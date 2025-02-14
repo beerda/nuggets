810ee50bc51252b66bdd077d806cfe2b0fa218da
Michal Burda <michal.burda@osu.cz>
2025-02-14 15:17:41 +0100

    Added test of Data

---
+++ src/test-dig-Data.cpp
@@ -0,0 +1,107 @@
+#include <testthat.h>
+#include "common.h"
+#include "dig/Data.h"
+#include "dig/BitChain.h"
+#include "dig/VectorNumChain.h"
+
+
+using DataType = Data<BitChain, VectorNumChain<GOGUEN>>;
+using DualChainType = DataType::DualChainType;
+
+context("dig/Data.h") {
+    test_that("creation") {
+        DataType data(10);
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 0);
+        expect_true(data.getCondition().size() == 0);
+        expect_true(data.getFoci().size() == 0);
+
+        data.addUnusedChain(); // 0
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 1);
+        expect_true(data.getCondition().size() == 0);
+        expect_true(data.getFoci().size() == 0);
+
+        data.addUnusedChain(); // 1
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 2);
+        expect_true(data.getCondition().size() == 0);
+        expect_true(data.getFoci().size() == 0);
+
+        LogicalVector data1(10);
+        LogicalVector data2(10);
+        NumericVector data3(10);
+        for (int i = 0; i < data1.size(); i++) {
+            data1[i] = (i == 2 || i == 5);
+            data2[i] = (i == 2 || i == 6);
+            data3[i] = i / 10.0;
+        }
+
+        data.addChain(data1, "d1", true, false); // 2
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 3);
+        expect_true(data.getCondition().size() == 1);
+        expect_true(data.getCondition()[0] == 2);
+        expect_true(data.getFoci().size() == 0);
+        expect_true(data.getName(2) == "d1");
+
+        data.addChain(data2, "d2", false, true); // 3
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 4);
+        expect_true(data.getCondition().size() == 1);
+        expect_true(data.getCondition()[0] == 2);
+        expect_true(data.getFoci().size() == 1);
+        expect_true(data.getFoci()[0] == 3);
+        expect_true(data.getName(2) == "d1");
+        expect_true(data.getName(3) == "d2");
+
+        data.addUnusedChain(); // 4
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 5);
+        expect_true(data.getCondition().size() == 1);
+        expect_true(data.getFoci().size() == 1);
+
+        data.addChain(data3, "d3", true, true); // 5
+        expect_true(data.nrow() == 10);
+        expect_true(data.size() == 6);
+        expect_true(data.getCondition().size() == 2);
+        expect_true(data.getCondition()[0] == 2);
+        expect_true(data.getCondition()[1] == 5);
+        expect_true(data.getFoci().size() == 2);
+        expect_true(data.getFoci()[0] == 3);
+        expect_true(data.getFoci()[1] == 5);
+        expect_true(data.getName(2) == "d1");
+        expect_true(data.getName(3) == "d2");
+        expect_true(data.getName(5) == "d3");
+
+        expect_true(data.getPositiveChain(0).empty());
+        expect_true(data.getPositiveChain(1).empty());
+        expect_true(!data.getPositiveChain(2).empty());
+        expect_true(!data.getPositiveChain(3).empty());
+        expect_true(data.getPositiveChain(4).empty());
+        expect_true(!data.getPositiveChain(5).empty());
+
+        expect_true(data.getNegativeChain(0).empty());
+        expect_true(data.getNegativeChain(1).empty());
+        expect_true(data.getNegativeChain(2).empty());
+        expect_true(data.getNegativeChain(3).empty());
+        expect_true(data.getNegativeChain(4).empty());
+        expect_true(data.getNegativeChain(5).empty());
+
+        data.initializeNegativeFoci();
+
+        expect_true(data.getPositiveChain(0).empty());
+        expect_true(data.getPositiveChain(1).empty());
+        expect_true(!data.getPositiveChain(2).empty());
+        expect_true(!data.getPositiveChain(3).empty());
+        expect_true(data.getPositiveChain(4).empty());
+        expect_true(!data.getPositiveChain(5).empty());
+
+        expect_true(data.getNegativeChain(0).empty());
+        expect_true(data.getNegativeChain(1).empty());
+        expect_true(data.getNegativeChain(2).empty());
+        expect_true(!data.getNegativeChain(3).empty());
+        expect_true(data.getNegativeChain(4).empty());
+        expect_true(!data.getNegativeChain(5).empty());
+    }
+}
