306dbe5ff091562fd37415022e32dee356a847a4
Michal Burda <michal.burda@osu.cz>
2025-02-25 05:27:17 +0100

    Simplified arules comparison

--- misc/arules_comparison.R
+++ misc/arules_comparison.R
@@ -13,29 +13,29 @@ testIt <- function(m, n) {
                 ncol = n)
     colnames(d) <- letters[seq_len(n)]
 
-    t1time <- 0
+    #t1time <- 0
     t2time <- 0
-    t3time <- 0
+    #t3time <- 0
     reps <- 5
+        #t1 <- system.time({
+            #fit <- apriori(d, parameter = list(minlen = 1,
+                                               #maxlen = 6,
+                                               #supp=0.001,
+                                               #conf = conf),
+                           #control = list(verbose = FALSE))
+            #rules1 <- DATAFRAME(fit)
+        #})
+
+        #t3 <- system.time({
+            #freq <- eclat(d, parameter = list(minlen = 1,
+                                              #maxlen = 6,
+                                              #supp=0.001),
+                          #control = list(verbose = FALSE))
+            #fit <- ruleInduction(freq, conf = conf)
+            #rules3 <- DATAFRAME(fit)
+        #})
+
     for (x in 1:reps) {
-        t1 <- system.time({
-            fit <- apriori(d, parameter = list(minlen = 1,
-                                               maxlen = 6,
-                                               supp=0.001,
-                                               conf = conf),
-                           control = list(verbose = FALSE))
-            rules1 <- DATAFRAME(fit)
-        })
-
-        t3 <- system.time({
-            freq <- eclat(d, parameter = list(minlen = 1,
-                                              maxlen = 6,
-                                              supp=0.001),
-                          control = list(verbose = FALSE))
-            fit <- ruleInduction(freq, conf = conf)
-            rules3 <- DATAFRAME(fit)
-        })
-
         t2 <- system.time({
             rules2 <- dig_associations(d,
                                        min_support = 0.001,
@@ -44,18 +44,18 @@ testIt <- function(m, n) {
                                        min_confidence = conf)
         })
 
-        t1time <- t1time + t1["elapsed"]
+        #t1time <- t1time + t1["elapsed"]
         t2time <- t2time + t2["elapsed"]
-        t3time <- t3time + t3["elapsed"]
+        #t3time <- t3time + t3["elapsed"]
     }
 
     data.frame(nrow = m,
                ncol = n,
-               apriori_time = t1time / reps,
-               eclat_time = t3time / reps,
+               #apriori_time = t1time / reps,
+               #eclat_time = t3time / reps,
                nuggets_time = t2time / reps,
-               apriori_count = nrow(rules1),
-               eclat_count = nrow(rules3),
+               #apriori_count = nrow(rules1),
+               #eclat_count = nrow(rules3),
                nuggets_count = nrow(rules2))
 }
 
@@ -65,12 +65,12 @@ testIt <- function(m, n) {
 #print(c(nrow(rules1), nrow(rules2)))
 
 result <- data.frame()
-for (i in 5:7) {
+for (i in 6) {
     for (j in c(5, 10, 15)) {
         result <- rbind(result, testIt(10^i, j))
-        saveRDS(result, "comparison_result-2025-01-07.rds")
-        cat("\n---------------------------------------------------------------------\n")
-        print(result)
+        #saveRDS(result, "comparison_result-2025-02-20.rds")
+        #cat("\n---------------------------------------------------------------------\n")
+        #print(result)
     }
 }
 print(result)
