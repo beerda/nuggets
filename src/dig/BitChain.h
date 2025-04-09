0a2976b21afb5b30e5040a6c42d009c74395157d
Michal Burda <michal.burda@osu.cz>
2025-04-09 15:43:31 +0200

    Rewritten BitChain

--- src/dig/BitChain.h
+++ src/dig/BitChain.h
@@ -1,74 +1,214 @@
 #pragma once
 
+#include <bitset>
 #include "../common.h"
-#include "Bitset.h"
+#include "../AlignedVector.h"
 
 
 /**
- * Implementation of chain of bits based on the Bitset class, which realizes
- * a growable array of bits.
+ * Implementation of chain of bits.
  */
 class BitChain {
 public:
+    constexpr static size_t CHUNK_SIZE = 8 * sizeof(uintmax_t);
+
+    /**
+     * Default constructor.
+     */
     BitChain()
-        : cachedSum(0)
+        : data(),
+          n(0),
+          cachedSum(0)
     { }
 
+    /**
+     * Constructor with a specified size.
+     */
+    BitChain(size_t n)
+        : data(calculateCapacity(n)),
+          n(n),
+          cachedSum(0)
+    { }
+
+    /**
+     * Constructor with specified data from Rcpp.
+     */
     BitChain(const LogicalVector& vals)
-        : cachedSum(0)
+        : data(calculateCapacity(vals.size())),
+          n(vals.size()),
+          cachedSum(0)
     {
-        reserve(vals.size());
-        for (R_xlen_t i = 0; i < vals.size(); i++)
-            push_back(vals.at(i));
+        for (R_xlen_t i = 0; i < vals.size(); ++i) {
+            if (vals[i]) {
+                data[i / CHUNK_SIZE] |= (uintmax_t(1) << (i % CHUNK_SIZE));
+                ++cachedSum;
+            }
+        }
     }
+
+    /**
+     * Copy constructor.
+     */
+    BitChain(const BitChain& other)
+        : data(other.data),
+          n(other.n),
+          cachedSum(other.cachedSum)
+    { }
+
+    /**
+     * Move constructor.
+     */
+    BitChain(BitChain&& other) noexcept
+        : data(std::move(other.data)),
+          n(other.n),
+          cachedSum(other.cachedSum)
+    { }
+
+    /**
+     * Copy assignment operator.
+     */
+    BitChain& operator=(const BitChain& other)
+    {
+        if (this != &other) {
+            data = other.data;
+            n = other.n;
+            cachedSum = other.cachedSum;
+        }
+
+        return *this;
+    }
+
+    /**
+     * Move assignment operator.
+     */
+    BitChain& operator=(BitChain&& other) noexcept
+    {
+        if (this != &other) {
+            data = std::move(other.data);
+            n = other.n;
+            cachedSum = other.cachedSum;
+        }
+
+        return *this;
+    }
+
+    /**
+     * Comparison (equality) operator.
+     */
+    bool operator == (const BitChain& other) const
+    { return (n == other.n) && (cachedSum == other.cachedSum) && (data == other.data); }
+
+    /**
+     * Comparison (inequality) operator.
+     */
+    bool operator != (const BitChain& other) const
+    { return !(*this == other); }
 
     void clear()
     {
-        values.clear();
+        data.clear();
+        n = 0;
         cachedSum = 0;
     }
 
     void reserve(size_t size)
-    { values.reserve(size); }
+    { data.reserve(calculateCapacity(size)); }
+
+    size_t size() const
+    { return n; }
+
+    size_t nChunks() const
+    { return data.size(); }
+
+    bool empty() const
+    { return n == 0; }
+
+    bool isAligned() const
+    { return n % CHUNK_SIZE == 0; }
 
     void push_back(bool value)
     {
-        values.push_back(value);
-        if (value)
-            cachedSum++;
+        if (isAligned()) {
+            data.push_back(0);
+        }
+        if (value) {
+            data.back() |= (uintmax_t(1) << (n % CHUNK_SIZE));
+            ++cachedSum;
+        }
+        ++n;
     }
 
-    void negate()
+    bool at(size_t index) const
     {
-        values.negate();
-        cachedSum = values.size() - cachedSum;
+        if (index >= n) {
+            throw std::out_of_range("Bitset::at");
+        }
+
+        return (data[index / CHUNK_SIZE] >> (index % CHUNK_SIZE)) & 1;
     }
-
-    void conjunctWith(const BitChain& other)
-    {
-        values &= other.values;
-        cachedSum = values.getSum();
-    }
-
-    size_t size() const
-    { return values.size(); }
-
-    bool empty() const
-    { return values.empty(); }
 
     float getSum() const
     { return 1.0 * cachedSum; }
 
-    bool at(size_t i) const
-    { return values.at(i); }
+    void negate()
+    {
+        if (n > 0) {
+            for (size_t i = 0; i < n / CHUNK_SIZE; i++) {
+                data[i] = ~data[i];
+            }
 
-    bool operator == (const BitChain& other) const
-    { return values == other.values; }
+            for (size_t i = 0; i < n % CHUNK_SIZE; i++) {
+                data[data.size() - 1] ^= (((uintmax_t) 1) << i);
+            }
+        }
+        cachedSum = n - cachedSum;
+    }
 
-    bool operator != (const BitChain& other) const
-    { return !(*this == other); }
+    void conjunctWith(const BitChain& other)
+    {
+        if (n != other.n) {
+            throw std::out_of_range("Bitset::operator&=: incompatible sizes");
+        }
+        cachedSum = 0;
+        for (size_t i = 0; i < data.size(); i++) {
+            data[i] &= other.data[i];
+            cachedSum += countBits(data[i]);
+        }
+    }
+
+    const AlignedVector<uintmax_t>& getData() const
+    { return data; }
+
+    AlignedVector<uintmax_t>& getMutableData()
+    { return data; }
+
+    string toString() const
+    {
+        stringstream res;
+        res << "[n=" << n << "]";
+        for (size_t i = 0; i < n; ++i) {
+            res << (at(i) + 0);
+        }
+
+        return res.str();
+    }
+
 
 private:
-    Bitset values;
+    AlignedVector<uintmax_t> data;
+    size_t n;
     size_t cachedSum;
+
+    static size_t calculateCapacity(size_t size)
+    {
+        // bitwise ceiling, see: http://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
+        return (size + CHUNK_SIZE - 1) / CHUNK_SIZE;
+    }
+
+    static inline size_t countBits(uintmax_t chunk)
+    {
+        //return popcount(chunk);                               // C++20 version
+        return static_cast<uint8_t>(bitset<64>(chunk).count()); // C++98 version
+    }
+
 };
--- src/test-dig-BitChain.cpp
+++ src/test-dig-BitChain.cpp
@@ -3,6 +3,289 @@
 #include "dig/BitChain.h"
 
 context("dig/BitChain.h") {
+    test_that("push_back") {
+        BitChain b;
+
+        expect_true(b.empty());
+        expect_true(b.size() == 0);
+        expect_true(b.nChunks() == 0);
+        expect_true(b.getSum() == 0);
+
+        b.push_back(true);
+        expect_true(!b.empty());
+        expect_true(b.size() == 1);
+        expect_true(b.nChunks() == 1);
+        expect_true(b.getSum() == 1);
+
+        b.push_back(false);
+        expect_true(!b.empty());
+        expect_true(b.size() == 2);
+        expect_true(b.nChunks() == 1);
+        expect_true(b.getSum() == 1);
+
+        b.push_back(true);
+        expect_true(!b.empty());
+        expect_true(b.size() == 3);
+        expect_true(b.nChunks() == 1);
+        expect_true(b.getSum() == 2);
+
+        b.push_back(true);
+        expect_true(!b.empty());
+        expect_true(b.size() == 4);
+        expect_true(b.nChunks() == 1);
+        expect_true(b.getSum() == 3);
+
+        for (int i = 0; i < 100; ++i)
+            b.push_back(false);
+
+        b.push_back(true);
+
+        expect_true(!b.empty());
+        expect_true(b.size() == 105);
+        expect_true(b.nChunks() == (b.size() + BitChain::CHUNK_SIZE - 1) / BitChain::CHUNK_SIZE);
+        expect_true(b.getSum() == 4);
+
+        expect_true(b.at(0));
+        expect_true(!b.at(1));
+        expect_true(b.at(2));
+        expect_true(b.at(3));
+
+        for (int i = 0; i < 100; ++i)
+            expect_true(!b.at(4 + i));
+
+        expect_true(b.at(104));
+    }
+
+    test_that("create nonempty") {
+        BitChain b(200);
+
+        expect_true(b.size() == 200);
+        expect_true(b.getSum() == 0);
+
+        for (int i = 0; i < 200; ++i)
+            expect_true(!b.at(i));
+    }
+
+    test_that("initialize from LogicalVector") {
+        LogicalVector v(5);
+        v[0] = true;
+        v[1] = false;
+        v[2] = true;
+        v[3] = true;
+        v[4] = false;
+
+        BitChain b(v);
+        expect_true(b.size() == 5);
+        expect_true(b.getSum() == 3);
+        expect_true(b.at(0));
+        expect_true(!b.at(1));
+        expect_true(b.at(2));
+        expect_true(b.at(3));
+        expect_true(!b.at(4));
+        expect_true(b.getMutableData().capacity() == 1);
+        expect_true(b.nChunks() == 1);
+    }
+
+    test_that("reserve") {
+        BitChain b;
+
+        b.reserve(BitChain::CHUNK_SIZE * 2);
+        expect_true(b.size() == 0);
+        expect_true(b.getMutableData().capacity() == 2);
+
+        b.reserve(BitChain::CHUNK_SIZE * 5);
+        expect_true(b.size() == 0);
+        expect_true(b.getMutableData().capacity() == 5);
+
+        b.reserve(BitChain::CHUNK_SIZE * 5 + 1);
+        expect_true(b.size() == 0);
+        expect_true(b.getMutableData().capacity() == 6);
+    }
+
+    test_that("negate") {
+        BitChain b;
+
+        expect_true(b.size() == 0);
+        expect_true(b.getSum() == 0);
+
+        b.negate();
+
+        expect_true(b.size() == 0);
+        expect_true(b.getSum() == 0);
+
+        b.push_back(true);
+        b.push_back(false);
+        b.push_back(true);
+        b.push_back(true);
+        b.push_back(false);
+
+        expect_true(b.size() == 5);
+        expect_true(b.getSum() == 3);
+
+        b.negate();
+
+        expect_true(b.size() == 5);
+        expect_true(b.getSum() == 2);
+
+        expect_true(!b.at(0));
+        expect_true(b.at(1));
+        expect_true(!b.at(2));
+        expect_true(!b.at(3));
+        expect_true(b.at(4));
+    }
+
+    test_that("negate 10") {
+        BitChain b;
+        size_t limit = 10;
+
+        for (size_t i = 0; i < limit; ++i) {
+            b.push_back(i % 2 == 0);
+        }
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 == 0));
+        }
+
+        b.negate();
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 != 0));
+        }
+    }
+
+    test_that("negate 63") {
+        BitChain b;
+        size_t limit = 63;
+
+        for (size_t i = 0; i < limit; ++i) {
+            b.push_back(i % 2 == 0);
+        }
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 == 0));
+        }
+
+        b.negate();
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 != 0));
+        }
+    }
+
+    test_that("negate 64") {
+        BitChain b;
+        size_t limit = 64;
+
+        for (size_t i = 0; i < limit; ++i) {
+            b.push_back(i % 2 == 0);
+        }
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 == 0));
+        }
+
+        b.negate();
+
+        for (size_t i = 0; i < limit /2; ++i) {
+            expect_true(b.at(i) == (i % 2 != 0));
+        }
+    }
+
+    test_that("negate 65") {
+        BitChain b;
+        size_t limit = 64;
+
+        for (size_t i = 0; i < limit; ++i) {
+            b.push_back(i % 2 == 0);
+        }
+
+        for (size_t i = 0; i < limit; ++i) {
+            expect_true(b.at(i) == (i % 2 == 0));
+        }
+
+        b.negate();
+
+        for (size_t i = 0; i < limit /2; ++i) {
+            expect_true(b.at(i) == (i % 2 != 0));
+        }
+    }
+
+    test_that("conjunction") {
+        BitChain b1;
+        BitChain b2;
+
+        b1.push_back(true);
+        b1.push_back(false);
+        b1.push_back(true);
+        b1.push_back(true);
+        b1.push_back(false);
+
+        b2.push_back(true);
+        b2.push_back(true);
+        b2.push_back(false);
+        b2.push_back(true);
+        b2.push_back(false);
+
+        expect_true(b1.size() == 5);
+        expect_true(b1.getSum() == 3);
+
+        expect_true(b2.size() == 5);
+        expect_true(b2.getSum() == 3);
+
+        b1.conjunctWith(b2);
+
+        expect_true(b1.size() == 5);
+        expect_true(b1.getSum() == 2);
+
+        expect_true(b1.at(0));
+        expect_true(!b1.at(1));
+        expect_true(!b1.at(2));
+        expect_true(b1.at(3));
+        expect_true(!b1.at(4));
+    }
+
+    test_that("negation and conjunction") {
+        BitChain b1;
+        BitChain b2;
+
+        b1.push_back(true);
+        b1.push_back(false);
+        b1.push_back(true);
+        b1.push_back(true);
+        b1.push_back(false);
+
+        b2.push_back(true);
+        b2.push_back(true);
+        b2.push_back(false);
+        b2.push_back(true);
+        b2.push_back(false);
+
+        expect_true(b1.size() == 5);
+        expect_true(b1.getSum() == 3);
+
+        expect_true(b2.size() == 5);
+        expect_true(b2.getSum() == 3);
+
+        b1.negate();
+        b2.negate();
+
+        expect_true(b1.size() == 5);
+        expect_true(b1.getSum() == 2);
+        expect_true(b2.size() == 5);
+        expect_true(b2.getSum() == 2);
+
+        b1.conjunctWith(b2);
+
+        expect_true(b1.size() == 5);
+        expect_true(b1.getSum() == 1);
+
+        expect_true(!b1.at(0));
+        expect_true(!b1.at(1));
+        expect_true(!b1.at(2));
+        expect_true(!b1.at(3));
+        expect_true(b1.at(4));
+    }
+
     test_that("complex test") {
         BitChain b;
 
