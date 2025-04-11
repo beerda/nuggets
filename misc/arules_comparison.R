6ab2918ae184af24dff08c05be39e6cfc06c7990
Michal Burda <michal.burda@osu.cz>
2025-04-11 06:29:01 +0200

    arules comparison analysis

--- misc/arules_comparison.R
+++ misc/arules_comparison.R
@@ -1,3 +1,4 @@
+library(tidyverse)
 library(arules)
 library(nuggets)
 
@@ -13,50 +14,58 @@ testIt <- function(m, n) {
                 ncol = n)
     colnames(d) <- letters[seq_len(n)]
 
-    #t1time <- 0
+    t1time <- 0
     t2time <- 0
-    #t3time <- 0
-    reps <- 5
-        #t1 <- system.time({
-            #fit <- apriori(d, parameter = list(minlen = 1,
-                                               #maxlen = 6,
-                                               #supp=0.001,
-                                               #conf = conf),
-                           #control = list(verbose = FALSE))
-            #rules1 <- DATAFRAME(fit)
-        #})
-
-        #t3 <- system.time({
-            #freq <- eclat(d, parameter = list(minlen = 1,
-                                              #maxlen = 6,
-                                              #supp=0.001),
-                          #control = list(verbose = FALSE))
-            #fit <- ruleInduction(freq, conf = conf)
-            #rules3 <- DATAFRAME(fit)
-        #})
-
+    t3time <- 0
+    reps <- 1
     for (x in 1:reps) {
-        t2 <- system.time({
-            rules2 <- dig_associations(d,
-                                       min_support = 0.001,
-                                       min_length = 0,
-                                       max_length = 5,
-                                       min_confidence = conf)
+        t1 <- system.time({
+            fit <- apriori(d, parameter = list(minlen = 1,
+                                               maxlen = 6,
+                                               supp=0.001,
+                                               target = "frequent itemsets"),
+                           control = list(verbose = FALSE))
+            rules1 <- DATAFRAME(fit)
         })
 
-        #t1time <- t1time + t1["elapsed"]
+        t3 <- system.time({
+            freq <- eclat(d, parameter = list(minlen = 1,
+                                              maxlen = 6,
+                                               target = "frequent itemsets",
+                                              supp=0.001),
+                          control = list(verbose = FALSE))
+            #fit <- ruleInduction(freq, conf = conf)
+            rules3 <- DATAFRAME(fit)
+        })
+
+        t2 <- system.time({
+            f <- function(condition) list(condition = condition)
+            rules2 <- dig(d,
+                          f = f,
+                          min_support = 0.001,
+                          min_length = 0,
+                          max_length = 5,
+                          min_focus_support = 0.001)
+            #rules2 <- dig_associations(d,
+                                       #min_support = 0.001,
+                                       #min_length = 0,
+                                       #max_length = 5,
+                                       #min_confidence = conf)
+        })
+
+        t1time <- t1time + t1["elapsed"]
         t2time <- t2time + t2["elapsed"]
-        #t3time <- t3time + t3["elapsed"]
+        t3time <- t3time + t3["elapsed"]
     }
 
     data.frame(nrow = m,
                ncol = n,
-               #apriori_time = t1time / reps,
-               #eclat_time = t3time / reps,
+               apriori_time = t1time / reps,
+               eclat_time = t3time / reps,
                nuggets_time = t2time / reps,
-               #apriori_count = nrow(rules1),
-               #eclat_count = nrow(rules3),
-               nuggets_count = nrow(rules2))
+               apriori_count = nrow(rules1),
+               eclat_count = nrow(rules3),
+               nuggets_count = length(rules2))
 }
 
 
@@ -64,13 +73,44 @@ testIt <- function(m, n) {
 #print(head(rules2))
 #print(c(nrow(rules1), nrow(rules2)))
 
-result <- data.frame()
-for (i in 6) {
-    for (j in c(5, 10, 15)) {
+result <- NULL
+for (i in 4:6) {
+    for (j in c(5, 10, 15, 20, 25)) {
         result <- rbind(result, testIt(10^i, j))
-        #saveRDS(result, "comparison_result-2025-02-20.rds")
-        #cat("\n---------------------------------------------------------------------\n")
-        #print(result)
+        saveRDS(result, "comparison_result-2025-04-10.rds")
+        cat("\n---------------------------------------------------------------------\n")
+        print(result)
     }
 }
 print(result)
+
+longResult <- result |>
+    pivot_longer(cols = c("apriori_time", "eclat_time", "nuggets_time"),
+             names_to = "method",
+             values_to = "time") |>
+    mutate(method = recode(method,
+                     "apriori_time" = "apriori",
+                     "eclat_time" = "eclat",
+                     "nuggets_time" = "nuggets"),
+           method = factor(method, levels = c("nuggets", "apriori", "eclat")))
+
+ggplot(longResult) +
+    aes(x = nrow, y = time, color = method) +
+    geom_line() +
+    scale_x_log10() +
+    scale_y_log10() +
+    facet_wrap(~ncol) +
+    labs(title = "Comparison of apriori, eclat and nuggets",
+         x = "Number of rows",
+         y = "Time (seconds)",
+         color = "Method") +
+    theme_minimal()
+
+ggplot(longResult) +
+    aes(x = ncol, y = time, color = method) +
+    geom_line() +
+    scale_y_log10() +
+    facet_wrap(~nrow)
+
+fit <- lm(log(time) ~ log(nrow) + ncol + method, data = longResult)
+summary(fit)
