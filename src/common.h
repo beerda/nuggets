0a7891adf78fb979ffaaa9543885bd83632c1983
Michal Burda <michal.burda@osu.cz>
2025-04-11 11:09:11 +0200

    Added debug macros

--- src/common.h
+++ src/common.h
@@ -12,9 +12,16 @@ using namespace Rcpp;
 using namespace std;
 
 
-// fast ceiling of positive numbers
-// see: http://stackoverflow.com/questions/2745074/fast-ceiling-of-an-integer-division-in-c-c
-#define UNSIGNED_CEILING(a, b)  ((a) + (b) - 1) / (b)
+#ifndef DEBUG
+#    define DEBUG 1
+#endif
+
+#ifdef DEBUG
+#    define IF_DEBUG(x) x
+#else
+#    define IF_DEBUG(x)
+#endif
+
 
 #define EQUAL(a, b) (fabs((a) - (b)) < 1e-6)
 
