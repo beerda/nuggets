6f65207c0ecef9979fbbc0e9641d1e0400121ec2
Michal Burda <michal.burda@osu.cz>
2025-05-12 19:34:01 +0200

    Implement argument handling in CallbackCaller

--- src/dig/CallbackCaller.h
+++ src/dig/CallbackCaller.h
@@ -35,14 +35,15 @@ public:
         List args;
 
         processConditionArgument(args, chain);
+        processSumArgument(args, chain);
+        processSupportArgument(args, chain);
+        processIndicesArgument(args, chain);
+        processWeightsArgument(args, chain);
+        processFociSupportsArgument(args, chain, collection);
 
         try {
-            //*
             RObject callbackResult = callback(args);
             result.push_back(callbackResult);
-            /*/
-            result.push_back(NumericVector());
-            // */
         }
         catch (...) {
             throw runtime_error("Error in callback function");
@@ -79,6 +80,74 @@ private:
                 vals.names() = valNames;
             }
             args.push_back(vals, "condition");
+        }
+    }
+
+    void processSumArgument(List& args, const CHAIN& chain)
+    {
+        if (config.hasSumArgument()) {
+            NumericVector vals({ chain.getSum() });
+            args.push_back(vals, "sum");
+        }
+    }
+
+    void processSupportArgument(List& args, const CHAIN& chain)
+    {
+        if (config.hasSupportArgument()) {
+            NumericVector vals({ chain.getSum() / config.getNrow() });
+            args.push_back(vals, "support");
+        }
+    }
+
+    void processIndicesArgument(List& args, const CHAIN& chain)
+    {
+        if (config.hasIndicesArgument()) {
+            if (chain.getClause().empty()) {
+                LogicalVector vals(config.getNrow(), true);
+                args.push_back(vals, "indices");
+            }
+            else {
+                LogicalVector vals(config.getNrow());
+                for (size_t i = 0; i < chain.size(); ++i) {
+                    vals[i] = chain[i] > 0;
+                }
+                args.push_back(vals, "indices");
+            }
+        }
+    }
+
+    void processWeightsArgument(List& args, const CHAIN& chain)
+    {
+        if (config.hasWeightsArgument()) {
+            if (chain.getClause().empty()) {
+                NumericVector vals(config.getNrow(), 1.0);
+                args.push_back(vals, "weights");
+            }
+            else {
+                NumericVector vals(config.getNrow());
+                for (size_t i = 0; i < chain.size(); ++i) {
+                    vals[i] = static_cast<float>(chain[i]);
+                }
+                args.push_back(vals, "weights");
+            }
+        }
+    }
+
+    void processFociSupportsArgument(List& args, const CHAIN& chain, const ChainCollection<CHAIN>& collection)
+    {
+        if (config.hasFociSupportsArgument()) {
+            NumericVector vals(collection.focusCount());
+            CharacterVector valNames(collection.focusCount());
+            for (size_t i = 0; i < collection.focusCount(); ++i) {
+                const CHAIN& focus = collection[i + collection.firstFocusIndex()];
+                size_t predicate = focus.getClause().back();
+                vals[i] = focus.getSum() / config.getNrow();
+                valNames[i] = config.getChainName(predicate);
+            }
+            if (vals.size() > 0) {
+                vals.names() = valNames;
+            }
+            args.push_back(vals, "foci_supports");
         }
     }
 };
