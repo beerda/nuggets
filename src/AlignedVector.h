94dd2784d3e7a0a5e714c4638054bfc274f756eb
Michal Burda <michal.burda@osu.cz>
2025-05-12 05:54:12 +0200

    Proper alignment of AlignedVector

--- src/AlignedVector.h
+++ src/AlignedVector.h
@@ -4,5 +4,10 @@
 #include "AlignedAllocator.h"
 
 
+/*
+ * 128bit operations (SSE2) - align by 16
+ * 256bit operations (AVX2) - align by 32
+ * 512bit operations (AVX512) - align by 64
+ */
 template <typename TYPE>
-using AlignedVector = std::vector<TYPE, AlignedAllocator<TYPE, 512>>;
+using AlignedVector = std::vector<TYPE, AlignedAllocator<TYPE, 32>>;
