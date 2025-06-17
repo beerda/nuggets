e8254700f35cbae15a326f7b476544de9c1ad0b2
Michal Burda <michal.burda@osu.cz>
2025-06-17 08:25:48 +0200

    Switch to XSIMD AlignedAllocator

--- src/AlignedVector.h
+++ src/AlignedVector.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <vector>
-#include "AlignedAllocator.h"
+#include <xsimd/memory/xsimd_aligned_allocator.hpp>
 
 
 /*
@@ -10,4 +10,4 @@
  * 512bit operations (AVX512) - align by 64
  */
 template <typename TYPE>
-using AlignedVector = std::vector<TYPE, AlignedAllocator<TYPE, 512>>;
+using AlignedVector = std::vector<TYPE, xsimd::aligned_allocator<TYPE, 64>>;
