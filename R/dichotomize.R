ff919f75cef2aacd51c622a9131401fd5e8f8627
Michal Burda <michal.burda@osu.cz>
2025-10-03 07:59:33 +0200

    Removed deprecated functions

--- R/dichotomize.R
+++ R/dichotomize.R
@@ -39,50 +39,5 @@ dichotomize <- function(.data,
                         ...,
                         .keep = FALSE,
                         .other = FALSE) {
-    lifecycle::deprecate_warn("1.4.0", "dichotomize()", with = "partition()")
-
-    .must_be_data_frame(.data)
-    .must_be_flag(.keep)
-    .must_be_flag(.other)
-
-    sel <- enquos(what, ...)
-    sel <- lapply(sel, eval_select, .data)
-    sel <- unlist(sel)
-    emptydf <- as_tibble(data.frame(matrix(NA, nrow = nrow(.data), ncol = 0)))
-    call <- current_env()
-
-    res <- lapply(seq_along(sel), function(i) {
-        colname <- names(sel)[i]
-        colindex <- sel[i]
-        res <- emptydf
-        x <- .data[[colindex]]
-
-        if (is.logical(x)) {
-            res <- tibble(a=x, b=!x)
-            colnames(res) <- paste0(colname, "=", c("T", "F"))
-        } else if (is.factor(x)) {
-            res <- lapply(levels(x), function(lev) x == lev)
-            names(res) <- paste0(colname, "=", levels(x))
-            res <- as_tibble(res)
-        } else {
-            cli_abort(c("Unable to dichotomize column {.var {colname}}.",
-                       "i"="Column to dichotomize must be a factor or logical.",
-                       "x"="The column {.var {colname}} is a {.cls {class(x)}}."),
-                      call = call)
-        }
-
-        if (.keep) {
-            res <- cbind(.data[colindex], res)
-        }
-
-        res
-    })
-
-    res <- do.call(cbind, res)
-
-    if (.other) {
-        res <- cbind(.data[, -sel, drop = FALSE], res)
-    }
-
-    as_tibble(res)
+    lifecycle::deprecate_stop("1.4.0", "dichotomize()", with = "partition()")
 }
