62701050159e3084d06e5ef29bdfdd999f3c8d48
Michal Burda <michal.burda@centrum.cz>
2025-10-10 20:01:04 +0200

    Added counts of distinct conditions to rulebaseTable()

--- R/ui-rulebaseTable.R
+++ R/ui-rulebaseTable.R
@@ -1,6 +1,19 @@
-rulebaseTable <- function(rules) {
-    tags$table(class = "info-table", width = "100%",
-        tags$tr(tags$td("Number of rules:"), tags$td(nrow(rules))),
-        tags$tr(tags$td("Number of columns:"), tags$td(ncol(rules)))
-    )
+rulebaseTable <- function(rules, meta) {
+    conds <- meta[meta$type == "condition", , drop= FALSE]
+    args <- lapply(seq_len(nrow(conds)), function(i) {
+        data_name <- conds$data_name[i]
+        long_name <- tolower(conds$long_name[i])
+        uni <- rules[[data_name]]
+        uni <- length(unique(uni))
+        tags$tr(tags$td(paste0("Number of distinct ", long_name, "s:")),
+                tags$td(uni))
+    })
+
+    args <- c(list(class = "info-table",
+                   width = "100%",
+                   tags$tr(tags$td("Number of rules:"), tags$td(nrow(rules))),
+                   tags$tr(tags$td("Number of columns:"), tags$td(ncol(rules)))),
+              args)
+
+    do.call(tags$table, args)
 }
--- tests/testthat/test-ui-rulebaseTable.R
+++ tests/testthat/test-ui-rulebaseTable.R
@@ -1,19 +1,28 @@
 test_that("rulebaseTable", {
-    rules <- data.frame(a = 1:3, b = 4:6)
-    ui <- rulebaseTable(rules)
+    rules <- data.frame(a = 1:3,
+                        b = 4:6,
+                        cond = c("x", "y", "x"))
+    meta <- data.frame(data_name = c("a", "b", "cond"),
+                       long_name = c("A", "B", "foobar"),
+                       type = c("numeric", "numeric", "condition"),
+                       stringsAsFactors = FALSE)
+
+    ui <- rulebaseTable(rules, meta)
     html <- as.character(ui)
 
     expect_match(html, "^<table class=\"info-table\"")
     expect_match(html, "<td>Number of rules:</td>[^<]*<td>3</td>")
-    expect_match(html, "<td>Number of columns:</td>[^<]*<td>2</td>")
+    expect_match(html, "<td>Number of columns:</td>[^<]*<td>3</td>")
+    expect_match(html, "<td>Number of distinct foobars:</td>[^<]*<td>2</td>")
     expect_match(html, "</table>$")
 
     rules <- data.frame()
-    ui <- rulebaseTable(rules)
+    ui <- rulebaseTable(rules, meta)
     html <- as.character(ui)
 
     expect_match(html, "^<table class=\"info-table\"")
     expect_match(html, "<td>Number of rules:</td>[^<]*<td>0</td>")
     expect_match(html, "<td>Number of columns:</td>[^<]*<td>0</td>")
+    expect_match(html, "<td>Number of distinct foobars:</td>[^<]*<td>0</td>")
     expect_match(html, "</table>$")
 })
