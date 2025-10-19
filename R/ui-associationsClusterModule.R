4e8314919f842078d136b06e009803f9e48a702a
Michal Burda <michal.burda@osu.cz>
2025-10-19 14:39:35 +0200

    Merged

--- R/ui-associationsClusterModule.R
+++ R/ui-associationsClusterModule.R
@@ -1,4 +1,5 @@
 associationsClusterModule <- function(id, rules, meta, data) {
+    max_clusters <- 20
     measures <- meta$data_name[meta$type == "numeric"]
     selected_measure <- meta$data_name[which.max(meta$clustering_default)]
 
@@ -9,7 +10,7 @@ associationsClusterModule <- function(id, rules, meta, data) {
                         sliderInput(NS(id, "k"),
                                     "Number of clusters",
                                     min = 2,
-                                    max = 2,
+                                    max = max_clusters,
                                     value = 2,
                                     step = 1,
                                     width = "100%"),
@@ -30,6 +31,8 @@ associationsClusterModule <- function(id, rules, meta, data) {
                 fluidRow(
                     column(width = 12,
                         hr(),
+                        dataTableOutput(NS(id, "clusteringTable")),
+                        br(),
                         plotOutput(NS(id, "clusteringPlot"), height = "500px")
                     )
                 )
@@ -48,10 +51,10 @@ associationsClusterModule <- function(id, rules, meta, data) {
                     l <- lengthUniqueAnte()
                     updateSliderInput(session,
                                       "k",
-                                      max = min(20, l - 1))
+                                      max = min(max_clusters, l - 1))
                 })
 
-                output$clusteringPlot <- renderPlot({
+                clustering <- reactive({
                     req(input$k)
                     req(input$by)
                     req(input$algorithm)
@@ -63,10 +66,20 @@ associationsClusterModule <- function(id, rules, meta, data) {
                     sel <- selectionReactive()
                     d <- rules[sel, , drop = FALSE]
 
-                    clu <- cluster_associations(d,
-                                                n = input$k,
-                                                by = input$by,
-                                                algorithm = input$algorithm)
+                    cluster_associations(d,
+                                         n = input$k,
+                                         by = input$by,
+                                         algorithm = input$algorithm)
+                })
+
+                output$clusteringTable <- renderDT({
+                })
+
+                output$clusteringPlot <- renderPlot({
+                    clu <- clustering()
+                    if (is.null(clu)) {
+                        return(NULL)
+                    }
 
                     ggplot(clu) +
                         aes(x = as.factor(cluster),
