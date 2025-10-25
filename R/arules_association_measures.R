8407106dfe7f850d415e0e106d504e12d49a0267
Michal Burda <michal.burda@osu.cz>
2025-10-25 19:16:26 +0200

    Added license header to source files

--- R/arules_association_measures.R
+++ R/arules_association_measures.R
@@ -1,4 +1,22 @@
 #######################################################################
+# nuggets: An R framework for exploration of patterns in data
+# Copyright (C) 2025 Michal Burda
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#######################################################################
+
+#######################################################################
 # This file contains code adapted from the 'arules' package licensed
 # under GPL-2 or later.
 #
@@ -36,6 +54,7 @@
 #   You should have received a copy of the GNU General Public License along
 #   with this program; if not, write to the Free Software Foundation, Inc.,
 #   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#######################################################################
 
 .arules_association_measures <- list(
     cosine = function(counts) with(counts, n11 / sqrt(n1x * nx1)),
