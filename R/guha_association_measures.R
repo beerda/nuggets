eba1550ed617441b885105058e4e90d3588ef4ce
Michal Burda <michal.burda@osu.cz>
2025-10-30 14:16:50 +0100

    Added commented-out replacement for pbinom that can handle non-integer values

--- R/guha_association_measures.R
+++ R/guha_association_measures.R
@@ -16,15 +16,15 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #######################################################################
 
+# replacement for pbinom that can handle non-integer values
+#betabinom <- function(k, n, theta) {
+    #pbeta(1 - theta, shape1 = n - k, shape2 = k + 1)
+#}
 
 .founded_impl <- function(a, b) a / (a + b)
 
 .lower_crit_impl <- function(a, b, p) pbinom(b, size = floor(a + b), prob = 1 - p)
 .upper_crit_impl <- function(a, b, p) pbinom(a, size = floor(a + b), prob = p)
-
-# pbinom(q, s, p) seems to equal pnbinom(s-q-1, q+1, p, lower.tail = FALSE)
-#.lower_crit_impl <- function(a, b, p) pnbinom(a-1, size = b+1, prob = 1 - p, lower.tail = FALSE)
-#.upper_crit_impl <- function(a, b, p) pnbinom(b-1, size = a+1, prob = p, lower.tail = FALSE)
 
 .guha_association_measure_names <- c(
     fi = "Founded Implication",
