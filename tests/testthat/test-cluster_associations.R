a087a093b589a7cf6a4a8cc2e81cf7d82ef09c37
Michal Burda <michal.burda@osu.cz>
2025-12-10 18:08:50 +0100

    Added test to cluster_associations() processing of single row rules

--- tests/testthat/test-cluster_associations.R
+++ tests/testthat/test-cluster_associations.R
@@ -42,9 +42,43 @@ test_that("cluster_associations()", {
 
     cluster_predicates <- attr(res, "cluster_predicates")
     cluster_size <- attr(res, "cluster_size")
+    expect_equal(length(cluster_predicates), 2)
     expect_equal(as.list(cluster_predicates[[1]]), list(a = 2, b = 2, c = 2))
     expect_equal(as.list(cluster_predicates[[2]]), list(d = 2, b = 1))
     expect_equal(cluster_size, c("1" = 3, "2" = 2))
+})
+
+
+test_that("cluster_associations() single row", {
+    d <- data.frame(antecedent = c("{a,b}"),
+                    consequent = c("{x}"),
+                    support = c(0.5),
+                    confidence = c(0.8),
+                    lift = c(1.5))
+    d <- nugget(d, "associations",
+                call_function = "dig_associations",
+                call_data = list(),
+                call_args = list())
+
+    expect_warning(
+        res <- cluster_associations(d, 1, lift, predicates_in_label = 3),
+        "The number of clusters `n` should be less than the number of distinct data points")
+    expect_true(is_tibble(res))
+    expect_equal(nrow(res), 1)
+    expect_equal(ncol(res), 6)
+    expect_equal(colnames(res), c("cluster", "cluster_label", "consequent", "support", "confidence", "lift"))
+    expect_equal(res$cluster, 1)
+    expect_equal(as.character(res$cluster_label), c("1 rules: {a, b}"))
+    expect_equal(as.character(res$consequent), c("{x}"))
+    expect_equal(res$support, c(0.5))
+    expect_equal(res$confidence, c(0.8))
+    expect_equal(res$lift, c(1.5))
+
+    cluster_predicates <- attr(res, "cluster_predicates")
+    cluster_size <- attr(res, "cluster_size")
+    expect_equal(length(cluster_predicates), 1)
+    expect_equal(as.list(cluster_predicates[[1]]), list(a = 1, b = 1))
+    expect_equal(cluster_size, c("1" = 1))
 })
 
 
