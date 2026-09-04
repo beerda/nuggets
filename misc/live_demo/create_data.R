e01038df233750411e2edc465223d1f784698b3c
Michal Burda <michal.burda@osu.cz>
2026-09-04 14:49:06 +0200

    Add more interest measures to live_demo

--- misc/live_demo/create_data.R
+++ misc/live_demo/create_data.R
@@ -1,7 +1,10 @@
 library(nuggets)
+library(dplyr)
 
 dataset <- partition(iris, .breaks = 3)
-rules <- dig_associations(dataset)
+rules <- dig_associations(dataset) |>
+    add_interest() |>
+    arrange(desc(confidence))
 
 saveRDS(list(dataset = dataset, rules = rules),
         file = "iris_data.rds")
